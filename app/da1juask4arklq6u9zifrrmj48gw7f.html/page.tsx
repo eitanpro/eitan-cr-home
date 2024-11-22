237919a8e633fdfe563cd91911b1393169f0c7ff
Eitan Kondratovsky <eitan.k@planet-nine.io>
2024-11-22 17:06:38 +0200

    Fix CD

---
+++ app/da1juask4arklq6u9zifrrmj48gw7f.html/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <>
+            da1juask4arklq6u9zifrrmj48gw7f
+        </>
+    )
+}
---
+++ components/WebsiteBuilder/QuestionsCollection.tsx
@@ -0,0 +1,37 @@
+export default function QuestionsCollection() {
+    const questions = [
+        {
+            question: "What is your email address?",
+        },
+        {
+            question: "What city and country are you located in?",
+        },
+        {
+            question: "What is the purpose of this website?",
+        },
+        {
+            question: "Are you satisfied with a static website offering a homepage, three service pages, a privacy policy, and a terms and conditions page?",
+        },
+        {
+            question: "Do you already own a domain you’d like us to connect? If yes, please provide the domain details. We always provide a free .pages.dev domain.",
+        },
+        {
+            question: "Please provide: A business description. Three main services and a paragraph for each (each will have a dedicated page). Your logo or confirm if you’d like us to design one for an additional 300 NIS?",
+        },
+        {
+            question: "What is the theme of your website?",
+        },
+        {
+            question: "Are you comfortable managing your website updates through the GitHub repository? Please note that we do not offer a maintenance service post-launch.",
+        },
+        {
+            question: "Would you like to include Google Analytics setup to monitor website traffic for an additional 300 NIS?",
+        },
+    ]
+
+    return (
+        <div>
+            <h1>Website Builder Questions</h1>
+        </div>
+    )
+}
